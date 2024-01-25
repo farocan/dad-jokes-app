e00d18258da0bfd4fe426574100d0f5422e84452
farocan <anna.fr.1990@gmail.com>
2024-01-25 09:28:46 +0100

    upvoting and downvoting jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Joke from "./Joke";
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import "./JokeList.css";
 
 class JokeList extends Component {
@@ -17,9 +19,17 @@ class JokeList extends Component {
             let res = await axios.get("https://icanhazdadjoke.com/", {
                 headers: {Accept: "application/json"}
             });
-            jokes.push(res.data.joke);
+            jokes.push({id: uuidv4(), text: res.data.joke, votes: 0});
         }
         this.setState({jokes: jokes});
+    }
+    handleVote(id, delta){
+        this.setState(
+            st => ({
+                jokes: st.jokes.map( j=>
+                    j.id ===id ? {...j, votes: j.votes + delta} : j
+                    )
+            }));
     }
     render(){
         return(
@@ -31,7 +41,13 @@ class JokeList extends Component {
                 </div>
                 <div className = "JokeList-jokes">
                     {this.state.jokes.map(j => (
-                        <div>{j}</div>
+                        <Joke 
+                        key={j.id} 
+                        votes={j.votes} 
+                        text={j.text} 
+                        upvote={()=> this.handleVote(j.id, 1)} 
+                        downvote={()=> this.handleVote(j.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
