ecff9f9fe4d4b451d3edd90265c1f987defd737b
farocan <anna.fr.1990@gmail.com>
2024-01-29 09:08:24 +0100

    adding a loading spinner

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,7 +10,10 @@ class JokeList extends Component {
     };
     constructor(props){
         super(props);
-        this.state = {jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]") };
+        this.state = {
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+            loading: false 
+        };
         this.handleClick = this.handleClick.bind(this);
     }
     async componentDidMount(){
@@ -24,10 +27,13 @@ class JokeList extends Component {
             });
             jokes.push({id: uuidv4(), text: res.data.joke, votes: 0});
         }
-        this.setState(st => ({
+        this.setState(
+            st => ({
+            loading: false,
             jokes: [...st.jokes, ...jokes]
         }),
-        () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        () => 
+            window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
         );
     }
     handleVote(id, delta){
@@ -41,9 +47,17 @@ class JokeList extends Component {
             );
     }
     handleClick(){
-        this.getJokes();
+        this.setState({loading: true}, this.getJokes)
     }
     render(){
+        if(this.state.loading){
+            return (
+                <div className="JokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin"/>
+                    <h1 className='JokeList-title'>Loading...</h1>
+                </div>
+            )
+        }
         return(
             <div className = "JokeList">
                 <div className="JokeList-sidebar">
