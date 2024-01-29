193f36e85b85126fc5f11c762397ef67f2430d0e
farocan <anna.fr.1990@gmail.com>
2024-01-29 08:57:46 +0100

    fixing LocalStorage bug

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,6 +11,7 @@ class JokeList extends Component {
     constructor(props){
         super(props);
         this.state = {jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]") };
+        this.handleClick = this.handleClick.bind(this);
     }
     async componentDidMount(){
         if(this.state.jokes.length ===0) this.getJokes();
@@ -23,8 +24,11 @@ class JokeList extends Component {
             });
             jokes.push({id: uuidv4(), text: res.data.joke, votes: 0});
         }
-        this.setState({jokes: jokes});
-        window.localStorage.setItem("jokes", JSON.stringify(jokes));
+        this.setState(st => ({
+            jokes: [...st.jokes, ...jokes]
+        }),
+        () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        );
     }
     handleVote(id, delta){
         this.setState(
@@ -32,7 +36,12 @@ class JokeList extends Component {
                 jokes: st.jokes.map( j=>
                     j.id ===id ? {...j, votes: j.votes + delta} : j
                     )
-            }));
+            }),
+            () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+            );
+    }
+    handleClick(){
+        this.getJokes();
     }
     render(){
         return(
@@ -40,7 +49,7 @@ class JokeList extends Component {
                 <div className="JokeList-sidebar">
                     <h1 className="JokeList-title"><span>Dad</span>Jokes</h1>
                     <img  src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-                    <button className="JokeList-getmore">New Jokes</button>
+                    <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 <div className = "JokeList-jokes">
                     {this.state.jokes.map(j => (
